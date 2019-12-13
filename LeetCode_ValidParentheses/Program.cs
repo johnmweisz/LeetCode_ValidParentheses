ed9d5dbf18412655f8d13da208c68129334bb4b5
John Weisz <johnmweisz@gmail.com>
2019-12-13 15:47:10 -0800

    Add project files.

---
+++ LeetCode_ValidParentheses/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_ValidParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.IsValid("{{}}"));
+        }
+    }
+    public class Solution
+    {
+        readonly Hashtable _hash = new Hashtable()
+        {
+            {'}', '{'},
+            {']', '['},
+            {')', '('}
+        };
+        readonly Stack<char> _stack = new Stack<char>();
+
+        public bool IsValid(string s)
+        {
+            foreach (char c in s)
+            {
+                if (_hash.ContainsValue(c))
+                {
+                    _stack.Push(c);
+                }
+                else
+                {
+                    if (_stack.Count == 0) return false;
+                    var removed = _stack.Pop();
+                    if ((char)_hash[c] != removed)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return _stack.Count == 0;
+        }
+    }
+}
